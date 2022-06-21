b81dedca7fce2a0e697a4c2e9c0f8b995428450a
Andraš <andras.tsitskan@gmail.com>
2022-06-21 10:09:27 +0300

    Complete challenge

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/main.js
@@ -0,0 +1,14 @@
+const divs = document.querySelectorAll("div");
+
+function logText(e) {
+  console.log(this.classList.value);
+  e.stopPropagation();
+}
+
+divs.forEach((div) =>
+  div.addEventListener("click", logText, { capture: false, once: true })
+);
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => console.log("Click!"), { once: true });
